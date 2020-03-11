8160bb035b7de6180191b9f62b506e17646c9135
JT Turner <jt.turner@moovel.com>
2020-03-11 15:50:27 -0700

    Update to latest bsb-native.

--- postinstall.js
+++ postinstall.js
@@ -1,15 +1,22 @@
-var exec = require('child_process').exec;
-var fs = require('fs');
-var path = require('path');
+"use strict";
 
-exec(require.resolve(path.join('bs-platform', 'lib', 'bsb.exe')), function(err, stdout, stderr) {
+const exec = require("child_process").exec;
+const fs = require("fs");
+const path = require("path");
+
+exec(path.join(__dirname, "bs-platform", "lib", "bsb.exe"), function(
+  err,
+  stdout,
+  stderr
+) {
   if (err) {
     console.error(stdout, stderr);
     return;
   }
-  
-  fs.createReadStream(path.join(__dirname, 'lib', 'bs', 'bytecode', 'matchenv'))
-    .pipe(fs.createWriteStream('matchenv'));
-  fs.chmodSync('matchenv', '755');
+
+  fs.createReadStream(
+    path.join(__dirname, "lib", "bs", "bytecode", "matchenv")
+  ).pipe(fs.createWriteStream("matchenv"));
+  fs.chmodSync("matchenv", "755");
   console.log("Successfully built matchenv!");
 });
